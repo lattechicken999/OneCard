a67e235cc265d01f1e19a23ab3e55cda9a4964a9
lattechicken999 <whdtn218@gmail.com>
2025-10-01 12:05:34 +0900

    [Develop] Card Enum에 조커 추가

--- OneCard/Enums.cs
+++ OneCard/Enums.cs
@@ -8,11 +8,11 @@ namespace OneCard
 {
     enum CardPattern
     {
-        None,Diamond, Heart, Club, Spade, end
+        None,Diamond, Heart, Club, Spade,Black,Color, end
     }
     enum CardNum
     {
-        _2, _3, _4, _5, _6, _7, _8, _9, _10, _J, _Q, _K, _A
+        _2, _3, _4, _5, _6, _7, _8, _9, _10, _J, _Q, _K, _A, _Jocker
     }
 
 }
